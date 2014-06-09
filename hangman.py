db443c938109441fbb621aa16342d381c5bb1c50
Brian Ruslim <bpr0422@gmail.com>
2014-06-09 16:08:15 -0400

    changed check from guesses to blanks

--- hangman.py
+++ hangman.py
@@ -6,7 +6,7 @@ max_wrong_guesses = 6
 
 def image(stage):
     """shows a various portions of a hanging man"""
-    print stage, max_wrong_guesses - stage, 'guessses left'
+    print stage, max_wrong_guesses - stage, ' guessses left'
 
 
 
@@ -17,16 +17,17 @@ def main():
     while True:
         image(len(guesses))
         print blanks
-        guess = raw_input('guess')
+        guess = raw_input('guess ')
         if guess in word:
             blanks = [letter if letter == guess else blank for blank, letter in zip(blanks, word)]
         else:
             guesses.append(guess)
+        
         if len(guesses) > max_wrong_guesses:
             image(len(guesses))
             print 'you lose!'
             return
-        if not None in guesses:
+        if not None in blanks:
             print 'you win!'
             return
 
